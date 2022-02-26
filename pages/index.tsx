95da84e8e31c226f5919b1bce28fcf07ef9d8a90
yue-fukahi <sou.yue.m11i@gmail.com>
2022-02-27 01:12:00 +0900

    ✨ Set interval to diceroll

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import { Container, Box, Grid, Button, selectClasses } from "@mui/material";
 import type { NextPage } from "next";
 import * as React from "react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Image from "next/image";
 
 const positionOpt = {
@@ -62,46 +62,75 @@ const DiceBox = () => {
     { faces: [Face.O, Face.Ha, Face.Ma, Face.Chi, Face.Ko, Face.Ko] },
   ];
 
-  const [dices, setDices] = useState(defaultDices);
+  const [dice0, setDice0] = useState<DiceProps>(defaultDices[0]);
+  const [dice1, setDice1] = useState<DiceProps>(defaultDices[1]);
+  const [dice2, setDice2] = useState<DiceProps>(defaultDices[2]);
+  const [dice3, setDice3] = useState<DiceProps>(defaultDices[3]);
+  const [dice4, setDice4] = useState<DiceProps>(defaultDices[4]);
   const [disabled, setDisabled] = useState(false);
 
-  const roll = () => {
-    setDices(defaultDices);
+  const reset = () => {
+    [setDice0, setDice1, setDice2, setDice3, setDice4].forEach((setDice, i) => {
+      setDice(defaultDices[i]);
+    });
+  };
+
+  const roll = (
+    dice: DiceProps,
+    setDice: (props: DiceProps) => void,
+    ms: number
+  ) => {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        const i = random(dice.faces.length);
+        resolve(setDice({ ...dice, selected: dice.faces[i] }));
+      }, ms);
+    });
+  };
+
+  const handleOnClick = () => {
+    reset();
     setDisabled(true);
 
-    setDices(
-      dices.map((dice) => {
-        const i = random(dice.faces.length);
-        return { ...dice, selected: dice.faces[i] };
-      })
-    );
-
-    setDisabled(false);
+    Promise.all([
+      roll(dice0, setDice0, 400 * 1),
+      roll(dice1, setDice1, 400 * 2),
+      roll(dice2, setDice2, 400 * 3),
+      roll(dice3, setDice3, 400 * 4),
+      roll(dice4, setDice4, 400 * 5),
+    ]).then(() => {
+      setDisabled(false);
+    });
   };
 
   return (
     <>
       <Grid container spacing={2}>
         <Grid item xs={4}>
-          <Dice {...dices[0]} />
+          <Dice {...dice0} />
         </Grid>
         <Grid item xs={4}>
-          <Dice {...dices[1]} />
+          <Dice {...dice2} />
         </Grid>
         <Grid item xs={4}>
-          <Dice {...dices[2]} />
+          <Dice {...dice4} />
         </Grid>
         <Grid item xs={2}></Grid>
         <Grid item xs={4}>
-          <Dice {...dices[3]} />
+          <Dice {...dice1} />
         </Grid>
         <Grid item xs={4}>
-          <Dice {...dices[4]} />
+          <Dice {...dice3} />
         </Grid>
         <Grid item xs={2}></Grid>
       </Grid>
       <Box sx={{ paddingTop: 3 }}>
-        <Button variant="outlined" size="large" onClick={roll} disabled={disabled}>
+        <Button
+          variant="outlined"
+          size="large"
+          onClick={handleOnClick}
+          disabled={disabled}
+        >
           おはまちこする
         </Button>
       </Box>
