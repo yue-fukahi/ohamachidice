01f3f36dcb43063762a705734d3ca83b7701f85c
yue-fukahi <sou.yue.m11i@gmail.com>
2022-02-28 15:46:06 +0900

    📦 v0.1.1

--- next.config.js
+++ next.config.js
@@ -1,5 +1,12 @@
 /** @type {import('next').NextConfig} */
+
+const urlPrefix = process.env.URL_PREFIX ? '/' + process.env.URL_PREFIX : '';
 const nextConfig = {
+  assetPrefix: urlPrefix,
+  basePath: urlPrefix,
+  trailingSlash: true,
+  publicRuntimeConfig: { urlPrefix },
+
   reactStrictMode: true,
 }
 
