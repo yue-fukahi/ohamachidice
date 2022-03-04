6c2467b5cdd562f9d1797ac8844b162d33559332
yue-fukahi <sou.yue.m11i@gmail.com>
2022-03-04 11:47:27 +0900

    🔧 Add settings for svgr

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
-/** @type {import('next').NextConfig} */
+/** @type {import("next").NextConfig} */
 
-const urlPrefix = process.env.URL_PREFIX ? '/' + process.env.URL_PREFIX : '';
+const urlPrefix = process.env.URL_PREFIX ? "/" + process.env.URL_PREFIX : "";
 const nextConfig = {
   assetPrefix: urlPrefix,
   basePath: urlPrefix,
@@ -8,6 +8,18 @@ const nextConfig = {
   publicRuntimeConfig: { urlPrefix },
 
   reactStrictMode: true,
+  images: {
+    disableStaticImages: true,
+  },
+
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    });
+
+    return config
+  },
 }
 
 module.exports = nextConfig
