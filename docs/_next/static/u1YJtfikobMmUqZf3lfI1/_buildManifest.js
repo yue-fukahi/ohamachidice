9a73d25bea242acfdb35825a4951af3d09f9a063
yue-fukahi <sou.yue.m11i@gmail.com>
2022-03-06 01:16:34 +0900

    📦 v0.2.2

---
+++ docs/_next/static/u1YJtfikobMmUqZf3lfI1/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/319-183baba1c28b9bc6.js","static/chunks/pages/index-aca81386e71f276c.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
