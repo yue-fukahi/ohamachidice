18041af030930563874afd307b1299b80b92bb3a
yue-fukahi <sou.yue.m11i@gmail.com>
2022-04-02 00:54:28 +0900

    Add editorconfig

--- src/constants/face.ts
+++ src/constants/face.ts
@@ -1,13 +1,13 @@
 const Face = {
-    O: "o",
-    Ha: "ha",
-    Ma: "ma",
-    Chi: "chi",
-    Ko: "ko",
-    Empty: "empty",
+  O: "o",
+  Ha: "ha",
+  Ma: "ma",
+  Chi: "chi",
+  Ko: "ko",
+  Empty: "empty",
 } as const;
 type Face = typeof Face[keyof typeof Face];
 
 export {
-    Face,
+  Face,
 }
