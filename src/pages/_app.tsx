817088f12d8ef2d6bdd045b4610d2573c3fe8b25
yue-fukahi <sou.yue.m11i@gmail.com>
2022-12-20 22:35:16 +0900

    ✨ Add custom theme

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,13 @@
-import '../../styles/globals.css'
-import type { AppProps } from 'next/app'
+import { ThemeProvider } from "@mui/material/styles";
+import type { AppProps } from "next/app";
+import { theme } from "../styles/theme";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function App({ Component, pageProps }: AppProps) {
+  return (
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
 
-export default MyApp
+export default App;
---
+++ src/styles/theme.ts
@@ -0,0 +1,13 @@
+import { createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+  typography: {
+    h1: {
+      color: '#263238',
+      fontSize: '3rem',
+      fontWeight: 100,
+    }
+  }
+});
+
+export { theme };
