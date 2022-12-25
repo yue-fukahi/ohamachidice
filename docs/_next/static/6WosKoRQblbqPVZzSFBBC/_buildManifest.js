ceabe6903769f0165686ca9cf108dc151396bbf6
yue-fukahi <sou.yue.m11i@gmail.com>
2022-12-25 19:39:11 +0900

    📦 v0.5.3

--- docs/_next/static/6WosKoRQblbqPVZzSFBBC/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/29107295-8619d3a1a46e5850.js","static/chunks/79-00c9870893f1fa7b.js","static/chunks/pages/index-b193c9384db167ba.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ docs/_next/static/QO4tuQvNft1JbIKQ1Wyr2/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/29107295-8619d3a1a46e5850.js","static/chunks/79-00c9870893f1fa7b.js","static/chunks/pages/index-1b178be7a6fdc5b0.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
