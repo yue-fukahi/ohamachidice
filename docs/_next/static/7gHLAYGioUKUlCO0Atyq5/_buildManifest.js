6c761b84b458367feb7c74aaf20478f878461f03
yue-fukahi <sou.yue.m11i@gmail.com>
2023-02-26 11:34:37 +0900

    📦 v1.0.0

--- docs/_next/static/7gHLAYGioUKUlCO0Atyq5/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/29107295-8619d3a1a46e5850.js","static/chunks/964-44ecf1ad679433d6.js","static/chunks/pages/index-dcdbda41ca3c09e5.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ docs/_next/static/ks8UXO_RciwwncN8X3j8a/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/29107295-8619d3a1a46e5850.js","static/chunks/964-44ecf1ad679433d6.js","static/chunks/pages/index-3b6109c1592d8a30.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
